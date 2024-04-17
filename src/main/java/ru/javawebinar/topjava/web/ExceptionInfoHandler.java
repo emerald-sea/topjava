db34c07b47d94bf460bc0aeb2834f8506abea340
emerald-sea <alrai.cepheus@gmail.com>
2024-04-17 09:47:32 +0100

    HW10. Remove unused code

--- src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
+++ src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
@@ -6,9 +6,7 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -57,7 +55,7 @@ public class ExceptionInfoHandler {
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorInfo handleMethodArgumentNotValidException(HttpServletRequest req, Exception e, MethodArgumentNotValidException error ) {
+    public ErrorInfo handleMethodArgumentNotValidException(HttpServletRequest req, Exception e) {
         return logAndGetErrorInfo(req, e, true, VALIDATION_ERROR);
     }
 

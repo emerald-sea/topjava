8330fefb2076bec5784cabad4ece8d9b865c75b3
emerald-sea <alrai.cepheus@gmail.com>
2024-04-17 01:37:27 +0100

    HW10. Validation for AdminUIController/MealUIController

--- src/main/java/ru/javawebinar/topjava/util/exception/ErrorInfo.java
+++ src/main/java/ru/javawebinar/topjava/util/exception/ErrorInfo.java
@@ -4,10 +4,12 @@ public class ErrorInfo {
     private final String url;
     private final ErrorType type;
     private final String detail;
+    private final String typeMessage;
 
     public ErrorInfo(CharSequence url, ErrorType type, String detail) {
         this.url = url.toString();
         this.type = type;
+        this.typeMessage = type.getMsg();
         this.detail = detail;
     }
 }
--- src/main/java/ru/javawebinar/topjava/util/exception/ErrorType.java
+++ src/main/java/ru/javawebinar/topjava/util/exception/ErrorType.java
@@ -1,8 +1,18 @@
 package ru.javawebinar.topjava.util.exception;
 
 public enum ErrorType {
-    APP_ERROR,
-    DATA_NOT_FOUND,
-    DATA_ERROR,
-    VALIDATION_ERROR
+    APP_ERROR("Internal Server Error"),
+    DATA_NOT_FOUND("Unprocessable Entity"),
+    DATA_ERROR("Conflict"),
+    VALIDATION_ERROR("Unprocessable Entity");
+
+    private final String msg;
+
+    ErrorType(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
 }
